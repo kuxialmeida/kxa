db893d11770d13bbfad3451c13218cbdf97e5569
Kuxixima Almeida <kuxialmeida@hotmail.com>
2024-09-26 05:32:49 +0100

    product show

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,19 @@ class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
   has_many_attached :photos
+  validate :photos_count
+
+
+
+
+
+  private
+
+  def photos_count
+
+    if photos.attached? && photos.length > 4
+      errors.add(:photos, 'You can upload a maximum of 4 photos.')
+    end
+  end
+
 end
